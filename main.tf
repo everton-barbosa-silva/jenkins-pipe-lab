313c6ef8479c7cb62326bb7804b5cb0dc8fd0501
everton-barbosa-silva <evbarbosa@uolinc.com>
2025-05-09 18:21:18 -0300

    primeiro commit com todos os arquivos

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins_target" {
-  ami                    = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
+  ami                    = "ami-0c7217cdde317cfec" 
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.default.key_name
   associate_public_ip_address = true
